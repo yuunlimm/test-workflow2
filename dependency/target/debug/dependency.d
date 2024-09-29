88d798357b2f906dd51ed16f061e451deedcf2dd
yuunlimm <dla0267@gmail.com>
2024-09-28 23:52:41 -0700

    add json

---
+++ dependency/target/debug/dependency.d
@@ -0,0 +1,1 @@
+/Users/yuunlim/test-workflow2/dependency/target/debug/dependency: /Users/yuunlim/test-workflow2/dependency/src/main.rs
---
+++ dependency/target/debug/deps/dependency-092efe41aa1b4828.d
@@ -0,0 +1,5 @@
+/Users/yuunlim/test-workflow2/dependency/target/debug/deps/dependency-092efe41aa1b4828: src/main.rs
+
+/Users/yuunlim/test-workflow2/dependency/target/debug/deps/dependency-092efe41aa1b4828.d: src/main.rs
+
+src/main.rs:
